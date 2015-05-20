d675a8ec75aa576b6291929f5c26e505c1dc11d1
zhenzhaoyang <zhenchy1@zhenchys-MacBook-Pro.local>
2015-05-20 10:31:06 +0800

    Initial Commit

---
+++ ShareUtility/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShareUtility
+//
+//  Created by zhenzhaoyang on 15/5/20.
+//  Copyright (c) 2015年 zhenchy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShareUtility/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShareUtility
+//
+//  Created by zhenzhaoyang on 15/5/20.
+//  Copyright (c) 2015年 zhenchy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
